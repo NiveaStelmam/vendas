9886f18f682846af7f3483358e36e9af1db17574
Nivea Stelmam <mand458@hotmail.com>
2024-08-28 19:23:51 -0300

    start

---
+++ src/test/java/spring/project/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
