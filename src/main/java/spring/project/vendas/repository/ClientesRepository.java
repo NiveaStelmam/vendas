32dfb1bc64f518c6573801b93a10ca7bf0096835
Nivea Stelmam <mand458@hotmail.com>
2024-09-04 18:13:53 -0300

    injeção de dependencia

--- src/main/java/spring/project/vendas/repository/ClientesRepository.java
+++ src/main/java/spring/project/vendas/repository/ClientesRepository.java
@@ -1,7 +1,10 @@
 package spring.project.vendas.repository;
 
+import org.springframework.stereotype.Repository;
 import spring.project.vendas.model.Cliente;
 
+
+@Repository
 public class ClientesRepository {
 
     public void persistir(Cliente cliente) {
--- src/main/java/spring/project/vendas/service/ClientesService.java
+++ src/main/java/spring/project/vendas/service/ClientesService.java
@@ -1,14 +1,23 @@
 package spring.project.vendas.service;
 
 
+import org.springframework.stereotype.Service;
 import spring.project.vendas.model.Cliente;
 import spring.project.vendas.repository.ClientesRepository;
 
+@Service
 public class ClientesService {
+
+    //  declarando variavel do tipo ClientesRepository
+    private final ClientesRepository clientesRepository;
+
+    // injeção de depéndencia usando construtor
+    public ClientesService(ClientesRepository clientesRepository){
+        this.clientesRepository = clientesRepository;
+    }
 
     public void salvarCliente(Cliente cliente){
         validarCliente(cliente);
-        ClientesRepository clientesRepository = new ClientesRepository();
         clientesRepository.persistir(cliente);
 
     }
