aafdcaab20e94014f58840871d21c4946a90c5b0
Nivea Stelmam <mand458@hotmail.com>
2024-09-10 20:33:29 -0300

    hml and prd profiles

--- src/main/java/spring/project/vendas/configuration/Config.java
+++ src/main/java/spring/project/vendas/configuration/Config.java
@@ -5,15 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import spring.project.vendas.Development;
+import spring.project.vendas.Production;
 
 
 // Anotação personalizada que herda o profile para e todas as configurações do perfil development
-@Development
+@Production
 //  A anotação @Profile("development")  deixa essa configuração disponivel apenas para o perfil "development"
 public class Config {
 
     @Bean
     public CommandLineRunner executar(){
-        return args -> System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        return args -> System.out.println("RODANDO A CONFIGURAÇÃO DE PRODUCAO");
     }
 }
